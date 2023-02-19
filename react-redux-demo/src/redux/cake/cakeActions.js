53ca45c0050791a4b81c60d5bf44bce39f04cd31
CHRISTOPHER <kichoraphael@gmail.com>
2023-02-19 16:08:45 +0530

    Add redux action-types, action-creators for `cake`

---
+++ react-redux-demo/src/redux/cake/cakeActions.js
@@ -0,0 +1,6 @@
+import { BUY_CAKE } from "./cakeTypes";
+
+// create action creator functions here
+export const buyCake = () => ({
+  type: BUY_CAKE,
+});
---
+++ react-redux-demo/src/redux/cake/cakeTypes.js
@@ -0,0 +1,2 @@
+// define action types as constants
+export const BUY_CAKE = "BUY_CAKE";
