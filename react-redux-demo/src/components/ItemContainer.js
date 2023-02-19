56500985b85fca080cd887a5020f6368068a08af
CHRISTOPHER <kichoraphael@gmail.com>
2023-02-19 22:23:36 +0530

    Use `ownProps` in mapDispatchToProps function

--- react-redux-demo/src/components/ItemContainer.js
+++ react-redux-demo/src/components/ItemContainer.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux";
 
 function ItemContainer(props) {
-  return <div>Item count: {props.itemCount}</div>;
+  return (
+    <div>
+      <h4>Item count: {props.itemCount}</h4>
+      <button onClick={props.buyItem}>Buy Item</button>
+    </div>
+  );
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -14,4 +20,13 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
