b9ed5482b4222bcc3c1884b106f50c7804bb68c5
CHRISTOPHER <kichoraphael@gmail.com>
2023-02-19 16:39:37 +0530

    Create store and connect it to the react app - wrap `App` component's return in `Provider` component from react-redux - pass the `store` as props to the `Provider` component

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
